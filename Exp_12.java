8ce41f0f6a2698cc21c6073f1b57c64c07fe0a4c
lakshmipriya <NEC0822135@nttf.co.in>
2024-07-16 09:28:24 +0530

    messages

---
+++ Exp_12.java
@@ -0,0 +1,13 @@
+package Apack;
+import pack3.*;
+class Exp_12//All
+{
+	public static void main(String args[])
+	{
+		C ob=new C();
+		ob.disp();
+		ob.dissp();
+		ob.disspp();
+		ob.dispp();
+	}
+}
