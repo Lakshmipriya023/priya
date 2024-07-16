44caa16e43b9e4940a2e4d34d637b84c0850df7e
priya <NEC0822135@nntf.co.in>
2024-07-16 10:57:30 +0530

    messages

---
+++ C.java
@@ -0,0 +1,20 @@
+package pack3;
+public class C
+{
+	private void disp()
+	{
+		System.out.println("this is private access specifier");
+	}
+	public void dispp()
+	{
+		System.out.println("this is public access specifier");
+	}
+	protected void dissp()
+	{
+		System.out.println("this is protected access specifier");
+	}
+	void disspp()
+	{
+		System.out.println("this is default access specifier");
+	}
+}
